572cdc2a020504944d4ab6a6243c53e805710a69
Daniel Opara <danielklys7@gmail.com>
2020-04-12 09:42:40 +0200

    error handling in api calls

--- src/script/api.js
+++ src/script/api.js
@@ -1,13 +1,23 @@
 export const fetchCompanies = async () => {
-  let response = await fetch(`https://recruitment.hal.skygate.io/companies`);
-  let data = await response.json();
-  return data;
+  try {
+    let response = await fetch(`https://recruitment.hal.skygate.io/companies`);
+    let data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
 };
 
 export const fetchIncomes = async (id) => {
-  let response = await fetch(`https://recruitment.hal.skygate.io/incomes/${id}`);
-  let data = await response.json();
-  return data;
+  try {
+    let response = await fetch(`https://recruitment.hal.skygate.io/incomes/${id}`);
+    let data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
 };
 
 
